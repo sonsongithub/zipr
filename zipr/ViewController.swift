a31bd66b9f28b8fb256088b81929f9d425a4949f
sonson <yoshida.yuichi@gmail.com>
2020-05-09 23:17:33 +0900

    Create new project.

---
+++ zipr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  zipr
+//
+//  Created by sonson on 2020/05/09.
+//  Copyright © 2020 sonson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
