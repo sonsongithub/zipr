21181541b37fe005761adbf28d12b4071a9be744
sonson <yoshida.yuichi@gmail.com>
2020-05-23 00:15:33 +0900

    Fixed the algorithm to order file paths.

--- zipr/Archiver.swift
+++ zipr/Archiver.swift
@@ -21,6 +21,95 @@ class ArchiverTask {
     }
 }
 
+private let regex: NSRegularExpression! = {
+    do {
+        return try NSRegularExpression(pattern: "^[^\\.]+.?\\.(jpg|gif|png|jpeg)", options: .caseInsensitive)
+    } catch {
+        assert(false, "Fatal error: \(#file) \(#line) \(error)")
+        return nil
+    }
+}()
+
+private let regex_extract_num: NSRegularExpression! = {
+    do {
+        return try NSRegularExpression(pattern: "(\\d+)", options: .caseInsensitive)
+    } catch {
+        assert(false, "Fatal error: \(#file) \(#line) \(error)")
+        return nil
+    }
+}()
+
+struct EntryInfo {
+    let entry: Entry
+    let unescapedFilePath: String
+}
+
+struct EntryKeyPair {
+    let entry: Entry
+    let key: String
+}
+
+func extractMaximumIntegerAmongFilePaths(_ entryInfos: [EntryInfo]) -> Int {
+    
+    let integers = entryInfos.map { (entryInfo) -> [Int] in
+        let matches = regex_extract_num.matches(in: entryInfo.unescapedFilePath, range: NSRange(location: 0, length: entryInfo.unescapedFilePath.count))
+        let a:[Int] = matches.map { (result) -> String in
+            return substring(entryInfo.unescapedFilePath, from: result)
+        }
+        .compactMap { (string) -> Int? in
+            return Int(string)
+        }
+        
+        return a
+    }
+    
+    let a: [Int] = Array(integers.joined())
+    
+    return a.max() ?? 1
+}
+
+func substring(_ string: String, from result: NSTextCheckingResult) -> String {
+    let from = string.index(string.startIndex, offsetBy: result.range.location)
+    let to = string.index(string.startIndex, offsetBy: result.range.location + result.range.length - 1)
+    return String(string[from...to])
+}
+
+func extractAllIntegerFromString(_ string: String) -> Int? {
+    return nil
+}
+
+func orderByExtractedIntegerAmongFilePath(_ entryInfos: [EntryInfo]) -> [Entry] {
+
+    let maximum_integer = extractMaximumIntegerAmongFilePaths(entryInfos)
+    
+    let digit_count = Int(log10(Double(maximum_integer))) + 2
+    
+    let format = String(format: "%%0%dd", digit_count)
+    
+    return entryInfos.map { (entryInfo) -> EntryKeyPair in
+        let matches = regex_extract_num.matches(in: entryInfo.unescapedFilePath, range: NSRange(location: 0, length: entryInfo.unescapedFilePath.count))
+        
+        let key = matches.map { (result) -> String in
+            return substring(entryInfo.unescapedFilePath, from: result)
+        }
+        .compactMap { (string) -> Int? in
+            return Int(string)
+        }
+        .map { (integer) -> String in
+            return String(format: format, integer)
+        }
+        .joined(separator: "_")
+        
+        return EntryKeyPair(entry: entryInfo.entry, key: key)
+    }
+    .sorted { (left, right) -> Bool in
+        return left.key < right.key
+    }
+    .map { (pair) -> Entry in
+        return pair.entry
+    }
+}
+
 class Archiver {
     let archive: Archive
     let entries: [Entry]
@@ -34,53 +123,46 @@ class Archiver {
     var taskQueue: [ArchiverTask] = Array([])
     var currentTask: ArchiverTask? = nil
     
+    
     init(_ fileURL: URL, identifier: String) throws {
         
         url = fileURL
         self.identifier = identifier
         
-        guard let tmp = Archive(url: fileURL, accessMode: .read) else {
+        guard let tmp = Archive(url: fileURL, accessMode: .read, preferredEncoding: String.Encoding.shiftJIS) else {
             throw NSError(domain: "", code: 0, userInfo: nil)
-        }
-        
-        guard let regex = try? NSRegularExpression(pattern: "^[^\\.]+.?\\.(jpg|gif|png|jpeg)", options: .caseInsensitive) else {
-            throw NSError(domain: "", code: 0, userInfo: nil)
-        }
-        guard let regex2 = try? NSRegularExpression(pattern: "[^\\d]", options: .caseInsensitive) else {
-            throw NSError(domain: "", code: 0, userInfo: nil)
-        }
-        
-        func get_file_number(url: URL) -> String {
-            let file_name = NSMutableString(string: url.lastPathComponent)
-            let length = file_name.lastPathComponent.count
-            regex2.replaceMatches(in: file_name, options: .reportProgress, range: NSRange(location: 0, length: length), withTemplate: "")
-            return file_name as String
         }
         
         archive = tmp
-        entries = archive.enumerated()
+        
+        let entryInfos: [EntryInfo] = archive.enumerated()
             .map { (offset, element) -> Entry in
-                print(element.path)
                 return element
             }
-            .filter({ (e) -> Bool in
-                let urlPath = URL(fileURLWithPath: e.path)
-                let length = urlPath.lastPathComponent.count
-                let matches = regex.matches(in: urlPath.lastPathComponent, range: NSRange(location: 0, length: length))
+            .filter { (entry) -> Bool in
+                let urlPath = URL(fileURLWithPath: entry.path)
+                let matches = regex.matches(in: urlPath.lastPathComponent, range: NSRange(location: 0, length: urlPath.lastPathComponent.count))
                 return (matches.count > 0)
-            })
-            .sorted(by: { (lhs, rhs) -> Bool in
-                let lhs_file = get_file_number(url: URL(fileURLWithPath: lhs.path))
-                let rhs_file = get_file_number(url: URL(fileURLWithPath: rhs.path))
-                
-                let lhs_int = Int(lhs_file) ?? 0
-                let rhs_int = Int(rhs_file) ?? 0
-                
-                print(lhs_file)
-                print(lhs_int)
-                
-                return (lhs_int < rhs_int)
-            })
+            }
+            .compactMap { (entry) -> EntryInfo? in
+                let urlPath = URL(fileURLWithPath: entry.path)
+                if let str = urlPath.lastPathComponent.removingPercentEncoding {
+                    return EntryInfo(entry: entry, unescapedFilePath: str)
+                }
+                return nil
+            }
+        
+        if entryInfos.count == 0 {
+            entries = archive.enumerated()
+                .sorted(by: { (left, right) -> Bool in
+                    return left.element.path < right.element.path
+                })
+                .map({ (e) -> Entry in
+                    return e.element
+                })
+        } else {
+            entries = orderByExtractedIntegerAmongFilePath(entryInfos)
+        }
         
         queue = DispatchQueue(label: "archiver")
     }
@@ -113,19 +195,16 @@ class Archiver {
                     d.append(data)
                 })
                 
-                print(d.count)
-                
-                guard let image = UIImage(data: d) else {
-                    return
+                if let image = UIImage(data: d) {
+                    
+                    let userInfo: [String: Any] = [
+                        "image": image,
+                        "page": tempCurrentTask.page,
+                        "identifier": self.identifier
+                    ]
+                    
+                    NotificationCenter.default.post(name: Notification.Name("Loaded"), object: nil, userInfo: userInfo)
                 }
-                
-                let userInfo: [String: Any] = [
-                    "image": image,
-                    "page": tempCurrentTask.page,
-                    "identifier": self.identifier
-                ]
-                
-                NotificationCenter.default.post(name: Notification.Name("Loaded"), object: nil, userInfo: userInfo)
                 
             } catch {
                 print("error")
